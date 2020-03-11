9958d173ba0feca6539f4fda51505a0cd8a32bb4
Hyeonho-Lee <prohenho7138@gmail.com>
2020-03-11 13:03:03 +0000

    upload files

---
+++ auto_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "===========업데이트를 시작하겠습니다.==========="
+python3 index.py
+
+echo "===========업로드 준비를 하겠습니다.==========="
+git status
+git add -A
+git commit -m "upload files"
+git push origin master
+
