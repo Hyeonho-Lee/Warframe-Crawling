13bab2be9a5611dc3a0bdb9ad8768f3adee2fbc0
PC R <prohenho7138@gmail.com>
2020-06-21 07:31:13 +0000

    update files

--- html/flask_app.py
+++ html/flask_app.py
@@ -666,4 +666,4 @@ def temp():
 #=======================================================================#
 if __name__ == '__main__':
     app.static_folder = 'static'
-    app.run(host = '0.0.0.0', port = '5000', debug=False)
+    app.run(host = '0.0.0.0', port = '8080', debug=False)
