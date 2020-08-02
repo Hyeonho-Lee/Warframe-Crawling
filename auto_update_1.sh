a1e4b16949c8445f7c8a6d2306d039b36d92707d
Hyeonho-Lee <prohenho7138@gmail.com>
2020-08-02 07:01:12 +0000

    update files

--- auto_update_1.sh
+++ auto_update_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /workspace/crawling
 date "+%Y/%m/%d/%H:%M" -d +9hours
-echo "업데이트를 시작하겠습니다."
+echo "디버깅 업데이트를 시작하겠습니다."
 echo "워프레임 업데이트 중입니다....."
 sleep 5
 /usr/local/bin/python3 /workspace/crawling/python/warframes_update.py
@@ -10,21 +10,6 @@ sleep 5
 echo "무기 업데이트 중입니다....."
 sleep 5
 /usr/local/bin/python3 /workspace/crawling/python/weapons_update.py
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/today_recommend.py
-echo "기타 무기 업데이트 중입니다....."
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/weapons_etc_update.py
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/today_recommend.py
-echo "모드 업데이트 중입니다....."
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/mods_update.py
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/today_recommend.py
-echo "아케인 업데이트 중입니다....."
-sleep 5
-/usr/local/bin/python3 /workspace/crawling/python/etcs_update.py
 sleep 5
 /usr/local/bin/python3 /workspace/crawling/python/today_recommend.py
 echo "결산 중입니다....."
